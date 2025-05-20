b0e08062c1e5dd28e5b7209e80b6541eab8917d1
mojet66 <oladosuemmanuel2017@gmail.com>
2025-05-20 14:05:11 +0100

    second commit

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,9 +1,13 @@
 const args = process.argv;
-const realArgs = args.slice(4);
-const count = realArgs.lenth;
+// console.log(args);
+const realArgs = args.slice(2);
+// console.log(realArgs);
+// console.log(realArgs.length);
+const count = realArgs.length;
+console.log(count);
 if (count === 0) {
   console.log("No argument");
-} else if (count === 0) {
+} else if (count === 1) {
   console.log("Argument found");
 } else {
   console.log("Arguments found");
