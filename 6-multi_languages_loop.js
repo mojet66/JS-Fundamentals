88f6534a7eeab6a4400e5b5c9e6c401e9b7591f4
mojet66 <oladosuemmanuel2017@gmail.com>
2025-05-22 17:41:14 +0100

    looping sentences

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,7 +1,11 @@
 const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
 
-let output = "";
-for (const line of lines) {
-  output += `${line}\n`;
+// let output = "";
+// for (const line of lines) {
+//   output += `${line}\n`;
+// }
+// console.log(output.trim());
+
+for (let i = 0; i < lines.length; i++) {
+  console.log(lines[i]);
 }
-console.log(output.trim());
