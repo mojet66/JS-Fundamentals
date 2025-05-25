fd793b3e76fe2135c951e609bc9447fdab5cd304
mojet66 <oladosuemmanuel2017@gmail.com>
2025-05-25 19:45:41 +0100

    factorials

---
+++ 10-factorial.js
@@ -0,0 +1,13 @@
+const arg = Number(process.argv[2]);
+
+function factorial(n) {
+  if (isNaN(n)) {
+    return 1;
+  }
+  if (n <= 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+console.log(factorial(arg));
